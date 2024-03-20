eab98aa87b81dbcc26263f20fc8ae6c53860086e
baltasarq <baltasarq@gmail.com>
2024-03-20 15:31:40 +0100

    Adds an about activity

--- app/src/main/java/com/devbaltasarq/cefaleapp/ui/AboutActivity.java
+++ app/src/main/java/com/devbaltasarq/cefaleapp/ui/AboutActivity.java
@@ -17,6 +17,7 @@ import android.webkit.WebView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
+import android.widget.TableLayout;
 import android.widget.TextView;
 
 import com.devbaltasarq.cefaleapp.R;
@@ -31,6 +32,7 @@ public class AboutActivity extends AppCompatActivity {
         final ActionBar ACTION_BAR = this.getSupportActionBar();
         final ImageView BT_ABOUT = this.findViewById( R.id.btAboutAbout );
         final ImageView BT_INFO = this.findViewById( R.id.btAboutInfo );
+        final ImageView BT_DETAIL = this.findViewById( R.id.btAboutDetailsInfo );
         final WebView LBL_APP_INFO = this.findViewById( R.id.lbl_app_info );
 
         if ( ACTION_BAR != null ) {
@@ -40,27 +42,44 @@ public class AboutActivity extends AppCompatActivity {
         }
 
         LBL_APP_INFO.loadUrl( "file:///android_asset/about.html" );
-        BT_ABOUT.setOnClickListener( v -> this.buttonPressed() );
-        BT_INFO.setOnClickListener( v -> this.buttonPressed() );
+        BT_ABOUT.setOnClickListener( this::buttonPressed );
+        BT_INFO.setOnClickListener( this::buttonPressed );
+        BT_DETAIL.setOnClickListener( this::buttonPressed );
+        this.buttonPressed( BT_DETAIL );
     }
 
-    private void buttonPressed()
+    private void buttonPressed(View v)
     {
         final ImageView BT_ABOUT = this.findViewById( R.id.btAboutAbout );
         final ImageView BT_INFO = this.findViewById( R.id.btAboutInfo );
+        final ImageView BT_DETAIL = this.findViewById( R.id.btAboutDetailsInfo );
         final LinearLayout LY_ABOUT = this.findViewById( R.id.lyAboutAbout );
         final WebView LY_INFO = this.findViewById( R.id.lbl_app_info );
+        final TableLayout LY_TABLE = this.findViewById( R.id.tblAppInfo );
 
-        if ( LY_INFO.getVisibility() == View.VISIBLE ) {
+        if ( v == BT_INFO ) {
             BT_INFO.setAlpha( 1f );
             BT_ABOUT.setAlpha( 0.25f );
-            LY_ABOUT.setVisibility( View.VISIBLE );
-            LY_INFO.setVisibility( View.GONE );
-        } else {
-            BT_INFO.setAlpha( 0.25f );
-            BT_ABOUT.setAlpha( 1f );
+            BT_DETAIL.setAlpha( 0.25f );
             LY_ABOUT.setVisibility( View.GONE );
             LY_INFO.setVisibility( View.VISIBLE );
+            LY_TABLE.setVisibility( View.GONE );
+        }
+        else
+        if ( v == BT_ABOUT ) {
+            BT_INFO.setAlpha( 0.25f );
+            BT_ABOUT.setAlpha( 1f );
+            BT_DETAIL.setAlpha( 0.25f );
+            LY_ABOUT.setVisibility( View.VISIBLE );
+            LY_INFO.setVisibility( View.GONE );
+            LY_TABLE.setVisibility( View.GONE );
+        } else {
+            BT_INFO.setAlpha( 0.25f );
+            BT_ABOUT.setAlpha( 0.25f );
+            BT_DETAIL.setAlpha( 1f );
+            LY_ABOUT.setVisibility( View.GONE );
+            LY_INFO.setVisibility( View.GONE );
+            LY_TABLE.setVisibility( View.VISIBLE );
         }
 
         return;
