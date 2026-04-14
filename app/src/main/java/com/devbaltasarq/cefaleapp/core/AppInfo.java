a79581fc6d76eee9984e61a963f257b34341f34b
baltasarq <baltasarq@gmail.com>
2026-04-14 20:07:28 +0200

    [BUMP] Version bump

--- app/src/main/java/com/devbaltasarq/cefaleapp/core/AppInfo.java
+++ app/src/main/java/com/devbaltasarq/cefaleapp/core/AppInfo.java
@@ -6,6 +6,6 @@ package com.devbaltasarq.cefaleapp.core;
 
 public class AppInfo {
     public static final String NAME = "CefaleaApp";
-    public static final String VERSION = "v3.6 20260223";
+    public static final String VERSION = "v3.7 20260414";
     public static final String FULL_NAME = NAME + " " + VERSION;
 }
